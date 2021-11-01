6521468e03d17ac4aa577dcc809075ace346c304
DMGithubPublisher <github@dynamsoft.com>
2021-11-01 13:06:19 +0800

    build related

--- samples/C++/Performance/BatchDecode/BarcodeStatisticsRecorder.h
+++ samples/C++/Performance/BatchDecode/BarcodeStatisticsRecorder.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "stdafx.h"
 #include <string>
 #include <list>
 #include<vector>
---
+++ samples/C++/Performance/BatchDecode/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <stdio.h>
+
+#if defined(_WIN64) || defined(_WIN32)
+#include "targetver.h"
+#include <tchar.h>
+#else
+#include <string.h>
+#include <strings.h>
+#endif
+
+
+// TODO: reference additional headers your program requires here
