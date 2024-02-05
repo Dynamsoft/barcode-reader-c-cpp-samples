4afea1e0d76d22ecd8654893634d53e0a762de76
DMGithubPublisher <github@dynamsoft.com>
2024-02-05 17:36:38 +0800

    add VideoDecoding sample

---
+++ Samples/VideoDecoding/VideoDecoding.cpp
@@ -0,0 +1,173 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <vector>
+#include <chrono>
+// Include headers of DynamsoftCaptureVisionRouter SDK
+#include <iostream>
+#include <string>
+
+#include "../../Include/DynamsoftCaptureVisionRouter.h"
+#include "../../Include/DynamsoftUtility.h"
+using namespace std;
+using namespace dynamsoft::license;
+using namespace dynamsoft::cvr;
+using namespace dynamsoft::dbr;
+using namespace dynamsoft::utility;
+using namespace cv;
+#if defined(_WIN64) || defined(_WIN32)
+#ifdef _WIN64
+#pragma comment(lib, "../../Distributables/Lib/Windows/x64/DynamsoftCorex64.lib")
+#pragma comment(lib, "../../Distributables/Lib/Windows/x64/DynamsoftLicensex64.lib")
+#pragma comment(lib, "../../Distributables/Lib/Windows/x64/DynamsoftCaptureVisionRouterx64.lib")
+#pragma comment(lib, "../../Distributables/Lib/Windows/x64/DynamsoftUtilityx64.lib")
+#else
+#pragma comment(lib, "../../Distributables/Lib/Windows/x86/DynamsoftCorex86.lib")
+#pragma comment(lib, "../../Distributables/Lib/Windows/x86/DynamsoftLicensex86.lib")
+#pragma comment(lib, "../../Distributables/Lib/Windows/x86/DynamsoftCaptureVisionRouterx86.lib")
+#pragma comment(lib, "../../Distributables/Lib/Windows/x86/DynamsoftUtilityx86.lib")
+#endif
+#endif
+
+
+class MyCapturedResultReceiver : public CCapturedResultReceiver {
+
+	virtual void OnDecodedBarcodesReceived(CDecodedBarcodesResult* pResult)override
+	{
+		if (pResult->GetErrorCode() != EC_OK)
+		{
+			cout << "Error: " << pResult->GetErrorString() << endl;
+		}
+		else
+		{
+			auto tag = pResult->GetOriginalImageTag();
+			if (tag)
+				cout << "ImageID:" << tag->GetImageId() << endl;
+			int count = pResult->GetItemsCount();
+			cout << "Decoded " << count << " barcodes" << endl;
+			for (int i = 0; i < count; i++) {
+				const CBarcodeResultItem* barcodeResultItem = pResult->GetItem(i);
+				if (barcodeResultItem != NULL)
+				{
+					cout << "Result " << i + 1 << endl;
+					cout << "Barcode Format: " << barcodeResultItem->GetFormatString() << endl;
+					cout << "Barcode Text: " << barcodeResultItem->GetText() << endl;
+				}
+			}
+		}
+
+		cout << endl;
+	}
+};
+
+class MyVideoFetcher : public CImageSourceAdapter
+{
+public:
+	MyVideoFetcher(){};
+	~MyVideoFetcher(){};
+	bool HasNextImageToFetch()const override
+	{
+		return true;
+	}
+	void MyAddImageToBuffer(const CImageData* img, bool bClone = true)
+	{
+		AddImageToBuffer(img, bClone);
+	}
+};
+
+int main()
+{
+	cout << "Opening camera..." << endl;
+	VideoCapture capture(0); // open the first camera
+	if (!capture.isOpened())
+	{
+		cerr << "ERROR: Can't initialize camera capture" << endl;
+		cout << "Press any key to quit..." << endl;
+		cin.ignore();
+		return 1;
+	}
+	int iRet = -1;
+	char szErrorMsg[256];
+	// Initialize license.
+  	// The string "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9" here is a free public trial license. Note that network connection is required for this license to work.
+  	// If you don't have a license yet, you can request a trial from https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=samples&package=c_cpp 
+   	iRet = CLicenseManager::InitLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", szErrorMsg, 256);
+	if (iRet != EC_OK)
+	{
+		cout << szErrorMsg << endl;
+	}
+	int errorCode = 1;
+	char errorMsg[512] = { 0 };
+
+	CCaptureVisionRouter *cvr = new CCaptureVisionRouter;
+
+	MyVideoFetcher *fetcher = new MyVideoFetcher();
+	fetcher->SetMaxImageCount(100);
+	fetcher->SetBufferOverflowProtectionMode(BOPM_UPDATE);
+	fetcher->SetColourChannelUsageType(CCUT_AUTO);
+	cvr->SetInput(fetcher);
+
+	CMultiFrameResultCrossFilter* filter = new CMultiFrameResultCrossFilter;
+	filter->EnableResultCrossVerification(CRIT_BARCODE, true);
+	filter->EnableResultDeduplication(CRIT_BARCODE, true);
+	filter->SetDuplicateForgetTime(CRIT_BARCODE, 5000);
+	cvr->AddResultFilter(filter);
+
+	CCapturedResultReceiver *capturedReceiver = new MyCapturedResultReceiver;
+	cvr->AddResultReceiver(capturedReceiver);
+
+	errorCode = cvr->StartCapturing(CPresetTemplate::PT_READ_BARCODES, false, errorMsg, 512);
+	if (errorCode != EC_OK)
+	{
+		cout << "error:" << errorMsg << endl;
+	}
+	else
+	{
+		for (int i = 1;; ++i)
+		{
+			auto start = std::chrono::high_resolution_clock::now();
+			Mat frame;
+			capture.read(frame);
+			if (frame.empty())
+			{
+				cerr << "ERROR: Can't grab camera frame." << endl;
+				break;
+			}
+			CFileImageTag tag(nullptr, 0, 0);
+			tag.SetImageId(i);
+			CImageData data(frame.rows * frame.step.p[0],
+				frame.data,
+				capture.get(CAP_PROP_FRAME_WIDTH),
+				capture.get(CAP_PROP_FRAME_HEIGHT),
+				frame.step.p[0],
+				IPF_RGB_888,
+				0,
+				&tag);
+			fetcher->MyAddImageToBuffer(&data);
+			imshow("Frame", frame);
+			auto finish = std::chrono::high_resolution_clock::now();
+			auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
+			int usingTime = elapsed.count();
+			int key = 1;
+			if (usingTime < 40)
+				key = 40 - usingTime;
+			key = waitKey(key);
+			if (key == 27/*ESC*/)
+				break;
+		}
+		cvr->StopCapturing();
+	}
+
+	capture.release();
+
+	delete cvr, cvr = NULL;
+	delete fetcher, fetcher = NULL;
+	delete filter, filter = NULL;
+	delete capturedReceiver, capturedReceiver = NULL;
+
+	cout << "Press any key to quit..." << endl;
+	cin.ignore();
+	return 0;
+
+}
---
+++ Samples/VideoDecoding/thirdLib/Include/opencv2/opencv_modules.hpp
@@ -0,0 +1,32 @@
+/*
+ *      ** File generated automatically, do not modify **
+ *
+ * This file defines the list of modules available in current build configuration
+ *
+ *
+*/
+
+// This definition means that OpenCV is built with enabled non-free code.
+// For example, patented algorithms for non-profit/non-commercial use only.
+/* #undef OPENCV_ENABLE_NONFREE */
+
+#undef HAVE_OPENCV_CALIB3D
+#define HAVE_OPENCV_CORE
+#undef HAVE_OPENCV_DNN
+#undef HAVE_OPENCV_FEATURES2D
+#undef HAVE_OPENCV_FLANN
+#define HAVE_OPENCV_HIGHGUI
+#undef HAVE_OPENCV_IMGCODECS
+#define HAVE_OPENCV_IMGPROC
+#undef HAVE_OPENCV_ML
+#undef HAVE_OPENCV_OBJDETECT
+#undef HAVE_OPENCV_PHOTO
+#undef HAVE_OPENCV_SHAPE
+#undef HAVE_OPENCV_STITCHING
+#undef HAVE_OPENCV_SUPERRES
+#undef HAVE_OPENCV_VIDEO
+#define HAVE_OPENCV_VIDEOIO
+#undef HAVE_OPENCV_VIDEOSTAB
+#undef HAVE_OPENCV_WORLD
+
+
